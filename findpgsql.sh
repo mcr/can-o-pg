158bea85563c8bd84edfd01b023b27da24851e8c
Michael Richardson <mcr@credil.org>
2016-03-27 14:00:12 -0400

    look for pg in order of versions

--- findpgsql.sh
+++ findpgsql.sh
@@ -4,7 +4,7 @@ POSTGRESQLlib=/usr/lib/postgresql
 
 if [ -x /ITER/bin/initdb ]; then echo /ITER/bin; exit; fi
 
-for pgdir in ${POSTGRESQLlib}/9.* ${POSTGRESQLlib}/8.*
+for pgdir in $(/bin/ls -1d ${POSTGRESQLlib}/9.* ${POSTGRESQLlib}/8.* 2>/dev/null | sort -nr)
 do
 	if [ -x ${pgdir}/bin/initdb ]; then echo ${pgdir}/bin; exit; fi
 done
